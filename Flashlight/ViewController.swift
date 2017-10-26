f7a79b79191eb6e56ab6fdd6e32813ddaccfe552
Masai Young <masai.young@gmail.com>
2017-10-26 17:33:52 -0400

    Submitting

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var flashlightButton: UIButton!
+    
+    @IBAction func flashlight() {
+        self.view.backgroundColor = self.view.backgroundColor == UIColor.white ? UIColor.black : UIColor.white
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
